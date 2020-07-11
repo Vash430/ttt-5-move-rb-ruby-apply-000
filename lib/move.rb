d7e509b741218e176d48364def86214bdd905016
essential-form-0157 <vasmith430@gmail.com>
2020-07-11 17:58:52 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,10 +8,3 @@ end
 
 # code your input_to_index and move method here!
 
-def input_to_index(user_input)
-   user_input.to_i - 4
-end
-
-def move(board, index, player)
-   board[index] = player
-end
